7d6e8e2d9f502902af379f63536a55164a074da7
Simon Tenbeitel <simon.tenbeitel@gmail.com>
2015-06-26 12:52:47 +0200

    Created MultiplechoiceQuestion object

---
+++ app/src/main/java/de/simontenbeitel/regelfragen/objects/MultiplechoiceQuestion.java
@@ -0,0 +1,36 @@
+package de.simontenbeitel.regelfragen.objects;
+
+/**
+ * @author Simon Tenbeitel
+ */
+public class MultiplechoiceQuestion extends Question {
+
+    public String[] answerPossibilities;
+    private int solutionIndex;
+    private int chosenAnswerIndex;
+
+    public MultiplechoiceQuestion(String text, long id, String[] answerPossibilities, int solutionIndex) {
+        super(text, id);
+        this.answerPossibilities = answerPossibilities;
+        this.solutionIndex = solutionIndex;
+    }
+
+    /**
+     * Save the index of the users answer
+     *
+     * @param index index of the chosen answer
+     * @return true if index is within the range
+     */
+    public boolean chooseAnswer(int index) {
+        if (0 > index || answerPossibilities.length <= index)
+            return false;
+        chosenAnswerIndex = index;
+        return true;
+    }
+
+    @Override
+    public double getFaults() {
+        return solutionIndex == chosenAnswerIndex ? 0 : 1;
+    }
+
+}
