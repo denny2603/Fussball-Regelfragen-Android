295975b64c807fac94e7e5f1f2b6630e78c9810f
Simon Tenbeitel <simon.tenbeitel@gmail.com>
2016-02-24 15:13:45 +0100

    Removed interactors, executors and threading to use Rx later

--- app/src/main/java/de/simontenbeitel/regelfragen/injection/component/ApplicationComponent.java
+++ app/src/main/java/de/simontenbeitel/regelfragen/injection/component/ApplicationComponent.java
@@ -3,12 +3,9 @@ package de.simontenbeitel.regelfragen.injection.component;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import de.simontenbeitel.regelfragen.domain.interactor.base.AbstractInteractor;
 
 @Singleton
 @Component
 public interface ApplicationComponent {
-
-    void inject(AbstractInteractor interactor);
 
 }
