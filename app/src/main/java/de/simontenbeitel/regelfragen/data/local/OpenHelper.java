f894957387f81b5009ef68a8469711b980bad139
Simon Tenbeitel <simon.tenbeitel@gmail.com>
2016-02-24 16:21:08 +0100

    DbOpenHelper

---
+++ app/src/main/java/de/simontenbeitel/regelfragen/data/local/OpenHelper.java
@@ -0,0 +1,32 @@
+package de.simontenbeitel.regelfragen.data.local;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import javax.inject.Inject;
+
+import de.simontenbeitel.regelfragen.injection.ApplicationContext;
+
+public class OpenHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "regelfragen.db";
+    public static final int DATABASE_VERSION = 1;
+
+    @Inject
+    public OpenHelper(@ApplicationContext Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Database.Question.CREATE);
+        db.execSQL(Database.MultipleChoiceAnswer.CREATE);
+        db.execSQL(Database.MatchSituationAnswer.CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
