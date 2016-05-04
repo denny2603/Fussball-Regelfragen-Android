c2cdefcee756237d116b944af689d76b6eb499ef
Simon Tenbeitel <simon.tenbeitel@sap.com>
2016-05-04 13:16:31 +0200

    Make class Frage serializable

--- app/src/main/java/de/simontenbeitel/regelfragen/Frage.java
+++ app/src/main/java/de/simontenbeitel/regelfragen/Frage.java
@@ -1,26 +1,29 @@
 package de.simontenbeitel.regelfragen;
 
-public class Frage{
-	public String fragentext;
-	protected double fehlerpunkte = 0;
-	protected boolean richtig_beantwortet = false;
-	private int typ = 0;
-	
-	public Frage(String fragentext, int typ)
-	{
-		this.fragentext = fragentext;
-		this.typ = typ;
-	}
-	
-	public boolean getRichtig()
-	{
-		return richtig_beantwortet;
-	}
-	public double getFehlerpunkte()
-	{
-		return fehlerpunkte;
-	}
-	public int getTyp(){
-		return typ;
-	}
+import java.io.Serializable;
+
+public class Frage implements Serializable {
+
+    public String fragentext;
+    protected double fehlerpunkte = 0;
+    protected boolean richtig_beantwortet = false;
+    private int typ = 0;
+
+    public Frage(String fragentext, int typ) {
+        this.fragentext = fragentext;
+        this.typ = typ;
+    }
+
+    public boolean getRichtig() {
+        return richtig_beantwortet;
+    }
+
+    public double getFehlerpunkte() {
+        return fehlerpunkte;
+    }
+
+    public int getTyp() {
+        return typ;
+    }
+
 }
